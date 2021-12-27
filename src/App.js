a5ba6460586d79747dcec222640d9de8505b371f
Juan Patricio Marroquin <review0312oficial@gmail.com>
2021-12-27 17:26:25 -0500

    Haciendo fetch a jsonPlaceholder de Usuarios

--- src/App.js
+++ src/App.js
@@ -6,22 +6,16 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: [
-        {
-          name: 'Frankestein',
-          id: 'asc1',
-        },
-        {
-          name: 'Dracula',
-          id: 'asr2',
-        },
-        {
-          name: 'Zombie',
-          id: 'as1w',
-        }
-      ],
+      monsters: [],
     }
   }
+
+componentDidMount() {
+  fetch('http://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({ monsters: users }));
+  
+}
   
   render() {
     return (
